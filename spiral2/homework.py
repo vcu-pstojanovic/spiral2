4a1c58045b3317336bb84511584f051e1211ccae
vcu-pstojanovic <78378662+vcu-pstojanovic@users.noreply.github.com>
2021-05-03 21:14:50 -0400

    Try again

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,10 @@
 def spiralize(size, n=1):
+    sum =[]
+    while topRight < size:
+      topRight = n**2 
+      topLeft = n**2 - n + 1 
+      bottomLeft = n**2 - 2*n + 2 
+      bottomRight = n**2 - 3*n + 3
 
     return_value= (3 + 2 * m * (8 * m * m + 15 * m + 13)) / 3
     return return_value
